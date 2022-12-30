1f5e1271da610ae060488c4b4e992d41f392057f
doniyor-eshmamatov <edoniyorbekdev@gmail.com>
2022-12-30 09:51:14 +0500

    First commit

---
+++ js/main.js
@@ -0,0 +1,50 @@
+let elForm = document.querySelector('.form')
+let elInput = document.querySelector('.input')
+let elList = document.querySelector('.list')
+let elTemplate = document.createElement('template')
+
+let elArr = [];
+
+
+
+function render(array, node) {
+    array.forEach(el => {
+        let elLi = document.createElement('li')
+        let elImg = document.createElement('img')
+        let elTitle = document.createElement('h3')
+        let elYear = document.createElement('p')
+
+        elImg.src = el.Poster;
+        elTitle.textContent = el.Title;
+        elYear.textContent = el.Year;
+
+        elLi.setAttribute('class', 'list-item')
+        elImg.setAttribute('class', 'item-img')
+        elTitle.setAttribute('class', 'item-title')
+        elYear.setAttribute('class', 'item-year')
+
+        elLi.appendChild(elImg)
+        elLi.appendChild(elTitle)
+        elLi.appendChild(elYear)
+        
+        node.appendChild(elTemplate);
+    });
+    
+}
+
+
+elForm.addEventListener('submit', evt => {
+    evt.preventDefault();
+
+    fetch(`http://www.omdbapi.com/?apikey=6e537db6&s=${elInput.value}`).then((response) => response.json()).then((data) => {
+    if (data) {
+        elArr = data.Search
+        console.log(elArr)
+        render(elArr, elList)
+    }
+})
+})
+
+
+
+
